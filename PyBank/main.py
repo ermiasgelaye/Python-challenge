38aec647237b055d5a76129e1343509830448cca
Ermias Gaga <ermiasgelaye@gmail.com>
2020-05-30 20:40:37 -0400

    first part of pybank

---
+++ PyBank/main.py
@@ -0,0 +1,22 @@
+import os
+import csv
+# Set relative path for csv file
+data_path=os.path.join('/Users/ermiasgaga/documents/GitHub/python-challenge/PyBank/', 'Resources', 'budget_data.csv')
+# counter for the total number of months
+total_months = 0
+
+# A counter for the total profit and loss
+total_profit_loss = 0
+
+# A counter for the output value of total profit and loss
+value = 0
+
+# A counter for the output value of total profit and loss
+change = 0
+
+# A list to hold the dates of the financial records
+dates = []
+
+# A list to hold the profits/loss
+profits = []
+
