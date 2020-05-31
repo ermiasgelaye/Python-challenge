def7e6ac0976ccea99fa5397a5f04309c000cb72
Ermias Gaga <ermiasgelaye@gmail.com>
2020-05-30 20:50:58 -0400

    reading the csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,3 +20,21 @@ dates = []
 # A list to hold the profits/loss
 profits = []
 
+# Read csv file
+with open(data_path, newline="") as budget_file:
+    csvreader = csv.reader(budget_file, delimiter=",")
+    # Reading header row
+    csv_header = next(csvreader)
+    # Go to the first row
+
+
+    first_row = next(csvreader)
+
+    total_months += 1
+
+    total_profit_loss += int(first_row[1])
+    value = int(first_row[1])
+    # Read the rows after the header row
+  
+
+
